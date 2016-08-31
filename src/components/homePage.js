1bd184e430ffde28d1adf399db47f1885616319d
AAKASH HANDA <ahanda205@gmail.com>
2016-08-31 21:24:23 +0530

    a

---
+++ src/components/homePage.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Home = React.createClass({
+	       render: function(){
+              return( 
+              		<div className="jumbotron">
+              			<h1> I am doing react </h1>
+              			<p> We are making author quiz </p>
+              		</div>
+              	);
+	       }
+});
+
+module.exports = Home;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+$ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+
+React.render(<Home />, document.getElementById('app'));
