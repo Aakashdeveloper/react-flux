39087416d85581a3a99dfea777b8ea54be8cc0f7
Aakash Handa <aakashh@aakashh1-linux.gur.corp.google.com>
2016-09-07 16:06:08 +0530

    a

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,6 +3,21 @@
 "use strict"
 var React = require('react');
  var About = React.createClass({
+ 	statics: {
+ 		willTransitionTo: function(transition, params, query, callback){
+ 			if(!confirm("are you sure you want to read")){
+ 				transition.about();
+ 			} else{
+ 				callback();
+ 			}
+ 		},
+ 		willTransitionFrom: function(transition, params, query, callback){
+ 			if(!confirm("are you want to read")){
+ 				transition.about();
+ 			} 
+ 		}
+
+ 	},
 	 render: function(){
 		 return (
 
